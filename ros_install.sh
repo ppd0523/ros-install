88c11f4caf71413f7de0ab86ab96441443f39bf1
Park donghyeon <dhpark@robocare.co.kr>
2020-06-10 11:37:52 +0900

    누락 명령어 추가. ROS 설치할 때 Y/n 안 나오게 하기

--- ros_install.sh
+++ ros_install.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-# pkg
+# Dependency
+sudo apt update && \
 sudo apt install -y git-core vim curl python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential
 
 # Setup sources.list
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list' && \
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654 && \
-sudo apt-get install ros-kinetic-desktop-full && \
-#sudo apt-get install ros-kinetic-desktop && \
-#sudo apt-get install ros-kinetic-ros-base && \
-git clone https://github.com/ppd0523/ros-install.git && \
+sudo apt update && \
+
+sudo apt-get install -y ros-kinetic-desktop-full && \
+#sudo apt-get install -y ros-kinetic-desktop && \
+#sudo apt-get install -y ros-kinetic-ros-base && \
+
 
 echo "source $HOME/.rosrc" >> ~/.bashrc && \
 cp -p .rosrc $HOME && \
